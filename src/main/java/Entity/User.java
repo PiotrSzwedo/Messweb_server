9afe8a4e9561441f0f4113a567c5db4bc4f3f7d1
Piotr Szwedo <>
2023-10-18 21:15:53 +0200

    I added a lost code

---
+++ src/main/java/Entity/User.java
@@ -0,0 +1,59 @@
+package Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String email;
+    @NonNull
+    private String username;
+
+    @NonNull
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
